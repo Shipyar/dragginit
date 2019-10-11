9e6b092258b3014533a07744bab5b69cec332817
Oliver Abraham <olly_abraham@hotmail.com>
2019-10-11 14:52:57 +0100

    Added Default Props
    This change adds default props to the draggable and droppable callback function to stop type errors

--- src/Draggable.js
+++ src/Draggable.js
@@ -12,18 +12,16 @@ const Draggable = React.memo(({ children, onDrag }) => {
   )
 })
 
+Draggable.defaultProps = {
+  onDrag: () => {} // Setting on drag to an empty function to prevent type error.
+}
+
 Draggable.propTypes = {
   /**
    * Called while dragging.
    * If this function returns the boolean false, dragging will be canceled.
    */
   onDrag: PropTypes.func,
-
-  /**
-   * Called when dragging stops.
-   * If this function returns the boolean false, the drag will remain active.
-   */
-  onStop: PropTypes.func,
 
   /**
    * React element passed down to the draggable compoent.
--- src/Droppable.js
+++ src/Droppable.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const Droppable = React.memo(({children, category, onDrop}) => {
+const Droppable = React.memo(({children, onDrop, canDrag, onDrag}) => {
   /**
    * Handle Drag Over,
-   * we need to revent default on the drag over otherwise drag wont work1
+   * we need to prevent default on the drag over otherwise drag wont work1
    */
   const handleDragOver = (e) => {
     e.preventDefault()
@@ -14,13 +14,21 @@ const Droppable = React.memo(({children, category, onDrop}) => {
     <div
       className='Droppable'
       style={{width: '100%', height: '100%'}}
-      onDrop={e => onDrop(e, category)}
+      draggable={canDrag}
+      onDrag={e => onDrag(e)}
+      onDrop={e => onDrop(e)}
       onDragOver={e => handleDragOver(e)}
     >
       {children}
     </div>
   )
 })
+
+Droppable.defaultProps = {
+  onDrop: () => {}, // Setting on drop to an empty function to prevent type error.
+  onDrag: () => {}, // Setting on drag to an empty function to prevent type error.
+  canDrag: false
+}
 
 Droppable.propTypes = {
   /**
@@ -29,11 +37,21 @@ Droppable.propTypes = {
    */
   onDrop: PropTypes.func,
 
-  category: PropTypes.string,
+  /**
+   * Called while dragging.
+   * If this function returns the boolean false, dragging will be canceled.
+   */
+  onDrag: PropTypes.func,
+
   /**
    * React element passed down to the draggable compoent.
    */
-  children: PropTypes.element
+  children: PropTypes.element,
+
+  /**
+   * Allow Droppable items to be dragged if this flag is set
+   */
+  canDrag: PropTypes.bool
 }
 
 export default Droppable
